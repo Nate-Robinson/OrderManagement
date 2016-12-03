d2dffca01cf9a90de6e4b0f6d2fb13f167959306
David <wuxiongbin@hotmail.com>
2016-12-03 23:32:36 +0800

    add api controller

--- OrderManagement/Global.asax.cs
+++ OrderManagement/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace OrderManagement
 {
@@ -13,6 +14,7 @@ namespace OrderManagement
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
