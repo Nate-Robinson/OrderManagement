91976be6602795e119078ff00982183fc0082eba
Jason <595724205@qq.COM>
2016-12-09 00:13:41 +0800

    feature 1. 测试登录！！！！！

--- OrderManagement/Controllers/LoginController.cs
+++ OrderManagement/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using OrderManagement.Common;
+using OrderManagement.Models;
 
 namespace OrderManagement.Controllers
 {
@@ -11,44 +13,89 @@ namespace OrderManagement.Controllers
     {
         //
         // GET: /Login/
-        public ActionResult Index()
+        public ActionResult StartLogin()
         {
             return View();
         }
 
-        [HttpPost, ActionName("Login")]
-        public void Login(FormCollection form)
+        [HttpPost]
+        public ActionResult StartLogin(FormCollection form, string returnUrl)
         {
-            //object obj = SqlHelper.ExecuteScalar("select UserId from CDBUsers where UserName=@uname and Password=@pwd",
-            //     new SqlParameter("@uname", collection[0]),
-            //     new SqlParameter("@pwd", Weibo.Models.Myencrypt.myencrypt(collection[1])));
+            if (!string.IsNullOrEmpty(form["user"]))
+            {
+                //FormsAuthentication.SetAuthCookie(uname,true);
+                string useraccount = form["user"];
+                string password = form["password"];
+                OrderManageDbContext db = new OrderManageDbContext();
+               // List<User> data = db.Users.Where(u => u.UserName == form["user"]).ToList();
+
+                bool userValid = db.Users.Any(user => user.Account == useraccount && user.PassWord == password);
+
+                //if (data.Count == 1)
+                //{
+                //    string RoleInfo = data[0].UserLevel == 1 ? "管理员": "普通用户"; 
+                //    FormsAuthenticationTicket ticket = new FormsAuthenticationTicket
+                //    (
+                //        1,                               // 版本号？？
+                //        form["user"],                    // 存储用户名
+                //        DateTime.Now,                    // 持续开始时间
+                //        DateTime.Now.AddMinutes(20),     // 持续结束时间
+                //        true,                            // 是否持久的
+                //        RoleInfo,                        // 登录用户信息，如：权限等级
+                //        "/"                              // 保存cookie的路径
+                //    );
+                //    var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, FormsAuthentication.Encrypt(ticket));
+                //    cookie.HttpOnly = true;
+                //    HttpContext.Response.Cookies.Add(cookie);
+                //    Response.Redirect(Request["ReturnUrl"]); // 重定向到用户申请的初始页面
+                //}
+                if (userValid)
+                {
+                    FormsAuthentication.SetAuthCookie(useraccount, false);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
 
 
-            //if (obj != null)
+
+            }
+            return View();
+            //if (form["user"].IsNullOrEmpty() || form["password"].IsNullOrEmpty())
             //{
+            //    //Response.Redirect("~");
+            //}
+            //if (form["user"] == null || form["password"] == null)
+            //{
+               
+            //}
+            //else
+            //{
+            //    FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+            //        1,
+            //        form[0],
+            //        DateTime.Now,
+            //        DateTime.Now.AddMinutes(30),
+            //        false,
+            //        "admins"
+            //        );
+
+            //    string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+            //    System.Web.HttpCookie authCookie = new System.Web.HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+            //    System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
+            //    Response.Redirect("~/home/index");
 
             //}
-            if (form["user"] == null || form["password"] == null)
-            {
-                Response.Redirect("~");
-            }
-            else
-            {
-                FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
-                    1,
-                    form[0],
-                    DateTime.Now,
-                    DateTime.Now.AddMinutes(30),
-                    false,
-                    "admins"
-                    );
-
-                string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
-                System.Web.HttpCookie authCookie = new System.Web.HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
-                System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
-                Response.Redirect("~/home/index");
-
-            }
 
         }
 	}
--- OrderManagement/Global.asax.cs
+++ OrderManagement/Global.asax.cs
@@ -6,11 +6,112 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Http;
+using System.Web.Security;
+using System.Security.Principal;
+using OrderManagement.Models;
 
 namespace OrderManagement
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        //protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        //{
+        //    var app = sender as HttpApplication;
+
+        //    if (app.Context.User != null)
+        //    {
+        //        var user = app.Context.User;
+        //        var identity = user.Identity as FormsIdentity;
+
+        //        // We could explicitly construct an Principal object with roles info using System.Security.Principal.GenericPrincipal
+        //        var principalWithRoles = new GenericPrincipal(identity, identity.Ticket.UserData.Split(','));
+
+        //        // Replace the user object
+        //        app.Context.User = principalWithRoles;
+
+        //    }
+        //}
+
+        protected void FormsAuthentication_OnAuthenticate(Object sender, FormsAuthenticationEventArgs e)
+        {
+            if (FormsAuthentication.CookiesSupported == true)
+            {
+                if (Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+                {
+                    try
+                    {
+                        //let us take out the username now                
+                        string username = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
+                        string roles = string.Empty;
+
+
+                        using (OrderManageDbContext db = new OrderManageDbContext())
+                        {
+                            User user = db.Users.SingleOrDefault(u => u.UserName == username);
+                            roles = user.UserLevel == 1? "管理员" : "普通用户";
+                        }
+                        //using (userDbEntities entities = new userDbEntities())
+                        //{
+                        //    User user = entities.Users.SingleOrDefault(u => u.username == username);
+
+                        //    roles = user.Roles;
+                        //}
+                        //let us extract the roles from our own custom cookie
+
+
+                        //Let us set the Pricipal with our user specific details
+                        e.User = new System.Security.Principal.GenericPrincipal(
+                          new System.Security.Principal.GenericIdentity(username, "Forms"), roles.Split(';'));
+                    }
+                    catch (Exception)
+                    {
+                        //somehting went wrong
+                    }
+                }
+            }
+        }
+
+
+       // protected void Application_AuthorizeRequest(object sender, System.EventArgs e)
+        //{
+
+         //   var id = Context.User.Identity as FormsIdentity;
+         //   if (id != null && id.IsAuthenticated)
+         //   {
+         //       var roles = id.Ticket.UserData.Split(',');
+         //       Context.User = new GenericPrincipal(id, roles);
+         //   }
+
+            //HttpApplication App = (HttpApplication)senderDefault1;
+            //HttpContext Ctx = App.Context; //获取本次Http请求相关的HttpContext对象
+
+            //if (Ctx.Request.IsAuthenticated == false) //验证过的用户才进行role的处理
+            //{
+            //    FormsIdentity Id = (FormsIdentity)Ctx.User.Identity;
+            //    FormsAuthenticationTicket Ticket = Id.Ticket; //取得身份验证票
+            //    string[] Roles = Ticket.UserData.Split(','); //将身份验证票中的role数据转成字符串数组
+            //    Ctx.User = new GenericPrincipal(Id, Roles); //将原有的Identity加上角色信息新建一个GenericPrincipal表示当前用户,这样当前用户就拥有了role信息
+            //}
+      //  }
+
+        //public MvcApplication()
+        //{
+        //    this.AuthorizeRequest += new EventHandler(MvcApplication_AuthorizeRequest);
+        //}
+
+        //void MvcApplication_AuthorizeRequest(object sender, EventArgs e)
+        //{
+        //    var id = Context.User.Identity as FormsIdentity;
+        //    if (id != null && id.IsAuthenticated)
+        //    {
+        //        var roles = id.Ticket.UserData.Split(',');
+        //        Context.User = new GenericPrincipal(id, roles);
+        //    }
+        //} 
+
+
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
